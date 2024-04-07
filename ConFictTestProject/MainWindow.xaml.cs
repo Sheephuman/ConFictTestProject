0c62c0871aac82d9b3d7896ac35c59713557cfc1
sheephuman <shin.citysheep@gmail.com>
2024-04-07 17:18:03 +0900

    2 test

--- ConFictTestProject/MainWindow.xaml.cs
+++ ConFictTestProject/MainWindow.xaml.cs
@@ -24,7 +24,7 @@ namespace ConFictTestProject
         private void Testbutton_Click(object sender, RoutedEventArgs e)
         {
             Testblock.Text = "プッシュテストを行います";
-            TestLabel.Content = "marge proceding....";
+            TestLabel.Content = "marge 2 proceding....";
         }   
     }
 }

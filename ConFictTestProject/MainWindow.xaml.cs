1943c331657200eb3c99779a0f46f830ba4adaca
sheephuman <shin.citysheep@gmail.com>
2024-04-07 16:11:22 +0900

    プロジェクト ファイルを追加します。

---
+++ ConFictTestProject/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConFictTestProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Testbutton_Click(object sender, RoutedEventArgs e)
+        {
+            Testblock.Text = "プッシュテストを行います";
+            TestLabel.Content = "proceding....";
+        }
+    }
+}
